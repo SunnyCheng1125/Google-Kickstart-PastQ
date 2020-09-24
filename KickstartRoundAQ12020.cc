d1e621002502ae3e96119f90b4fdbcedb514272f
SunnyCheng1125 <32073042+SunnyCheng1125@users.noreply.github.com>
2020-09-24 03:21:44 -0600

    Add files via upload

---
+++ KickstartRoundAQ12020.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int testCases, houseNum, budget, tmp;
+    vector<int> price, efHouse;
+    cin >> testCases;
+    int times = 0;
+    while (testCases--) {
+        cin >> houseNum >> budget;
+        for (int i = 0; i < houseNum; ++i) {
+            cin >> tmp;
+            price.push_back(tmp);
+        }
+        sort(price.begin(), price.end());
+        for (int j = 0; j < price.size(); j++) {
+            if (price[j] < budget) {
+                if (((accumulate(efHouse.begin(), efHouse.end(), 0)) + price[j]) <= budget) {
+                    efHouse.push_back(price[j]);
+                }
+            }
+        }
+        times += 1;
+        cout << "Case #" << times << ": ";
+        cout << efHouse.size() << '\n';
+        efHouse.clear();
+        price.clear();
+
+//        testCases--;
+    }
+    return 0;
+}
