6e73f14a55a5e3746ce60c2d28b77c73747376ef
SunnyCheng1125 <32073042+SunnyCheng1125@users.noreply.github.com>
2020-09-24 20:50:28 -0600

    Update KickstartRoundAQ12020.cc

--- KickstartRoundAQ12020.cc
+++ KickstartRoundAQ12020.cc
@@ -7,18 +7,33 @@ using namespace std;
 
 int main() {
     int testCases, houseNum, budget, tmp;
+    
+    //price for the input house price, EFhouse for affordable house price
     vector<int> price, efHouse;
     cin >> testCases;
     int times = 0;
+    
+    //decrease the testCases number by 1
     while (testCases--) {
         cin >> houseNum >> budget;
+        
+        //get the input prices of all houses prices
         for (int i = 0; i < houseNum; ++i) {
             cin >> tmp;
             price.push_back(tmp);
         }
+        
+        //sort the vector price, make the prices from low to high
         sort(price.begin(), price.end());
+        
+        //get the sum from the vector price
         for (int j = 0; j < price.size(); j++) {
+            
+            //check if the price[j] equal or less than to the budget
             if (price[j] <= budget) {
+                
+                //check if the current sum in the vector efhouse, and add the current price[j] will equal or less than the budget,
+                //if it's, add price[j] in the efhouse, else continue
                 if (((accumulate(efHouse.begin(), efHouse.end(), 0)) + price[j]) <= budget) {
                     efHouse.push_back(price[j]);
                 }
@@ -29,8 +44,6 @@ int main() {
         cout << efHouse.size() << '\n';
         efHouse.clear();
         price.clear();
-
-//        testCases--;
     }
     return 0;
 }
